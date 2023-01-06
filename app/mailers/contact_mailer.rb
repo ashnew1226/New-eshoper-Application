024957034d54e04f063cd1fb2a114f5808a0c368
ashnew1226 <ashishdede05@gmail.com>
2023-01-06 12:47:28 +0530

    update contact mailer for admin reply

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,12 @@
 class ContactMailer < ApplicationMailer
     def contact_mail(current_user)
-        binding.pry
         @user = current_user
         @contact = params[:contact]
         mail(to: @user.email, subject: "Your responce sent successfully.")
+    end
+
+    def contact_admin_mail(contact)
+        @contact = contact
+        mail(to: @contact.email, subject: "Your responce sent successfully.")
     end
 end
