24de931ca2ab113c8c2ab3ca0869c40f834e4d73
ashnew1226 <ashishdede05@gmail.com>
2023-01-09 14:27:42 +0530

    added method to implement weekly report email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,8 +7,13 @@ class UserMailer < ApplicationMailer
 
     def new_user_admin_email
         @user = User.find_by(superadmin_role: true,supervisor_role: true)
-        binding.pry
+        # binding.pry
         @user_last = User.last
         mail(to: @user.email, subject: "New user signed in")
+    end
+    def weekly_wishlist
+        @admin = User.find_by(superadmin_role: true,supervisor_role: true)
+        @total_products = Wishlist.where(created_at: (Time.now.midnight - 7.day)..Time.now.midnight) 
+        mail(to: @admin.email, subject: "weekly wishlist report.")
     end
 end
