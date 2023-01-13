8d9a576fce1dc8b29d91400bf86de032449066e2
ashnew1226 <ashishdede05@gmail.com>
2023-01-13 12:41:30 +0530

    validations for contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,10 @@
 class Contact < ApplicationRecord
     after_update :send_admin_responce
 
+    validates :name, :email,:message, presence: true
+    # PHONE_REGEX = /\A\(\d{3}\)\d{3}-\d{4}\z/
+    # validates :contact_no, format: { with: PHONE_REGEX }
+    validates :contact_no, length: { is: 10 }
     def send_admin_responce
         ContactMailer.contact_admin_mail(self).deliver_now
     end
