0abc6a788aa406a334cd070cf8d033ed7bc98a37
ashnew1226 <ashishdede05@gmail.com>
2023-02-02 15:13:38 +0530

    changes in redirection of page

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,11 +7,9 @@ class CartController < ApplicationController
     a = session[:cart] << id unless session[:cart].include?(id)
     respond_to do |format|
       if a != nil
-        format.html { redirect_to product_index_url(@product), notice: "Product added successfully." }
-        format.json { render :index, status: :created, location: @cart }
+        format.html { redirect_to request.referrer, notice: "Product added successfully." }
       else
         format.html { render :index, status: :unprocessable_entity }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
       end
     
     end
@@ -21,7 +19,7 @@ class CartController < ApplicationController
     id = params[:id].to_i
     session[:cart].delete(id)
     respond_to do |format|
-      format.html { redirect_to cart_index_url, notice: "product removed successfully." }
+      format.html { redirect_to request.referrer, notice: "product removed successfully." }
       format.json { head :no_content }
     end
   end
