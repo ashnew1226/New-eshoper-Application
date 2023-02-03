691310997dcbe16075a46604c5d4c976702838ab
ashnew1226 <ashishdede05@gmail.com>
2023-02-03 17:25:31 +0530

    changes in cart for increase decrease product quantity and display the price with coupon and coupons off amount

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,8 @@
 class CartController < ApplicationController
+  before_action :cart_price_with_shipping
   def index
+    @total_price_with_coupon = params[:total].to_i
+    @percent_off = params[:coupon_off].to_i
   end
 
   def create
@@ -23,4 +26,35 @@ class CartController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def decrease_quantity
+    @product_item = Product.find(params[:id])
+    @product_item.quantity -= 1
+    @product_item.save
+    redirect_to request.referrer
+  end
+
+  def increase_quantity
+      @product_item = Product.find(params[:id])
+      @product_item.quantity += 1
+      @product_item.save
+      redirect_to request.referrer
+  end
+
+  private
+
+
+  def cart_price_with_shipping
+    @cart_products = []
+    @cart.each do |product|
+        @total_price = (product.quantity)*(product.price).to_i
+        @cart_products << @total_price
+    end
+    @tp = @cart_products.inject {|sum,price| sum + price}
+    @cart_product_price = @tp.to_i
+    @cart_product_price > 500 ? shipping_charge = 0 : shipping_charge = 25
+    @applied_shipping_charge = shipping_charge
+    @products_price = @cart_product_price + shipping_charge
+  end
+  
 end
