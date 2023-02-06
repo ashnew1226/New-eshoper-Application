d3544caf5434992171cc35452ba95e68590e09a0
ashnew1226 <ashishdede05@gmail.com>
2023-02-06 09:21:15 +0530

    fetch records and show to show page for contacts

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,32 @@
+class ContactsController < ApplicationController
+  def show
+  end
+
+  # GET /contacts/new
+  def new
+    @contact = Contact.new
+  end
+
+
+
+  # POST /contacts or /contacts.json
+  def create
+    @contact = current_user.contacts.build(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to contact_url(@contact), notice: "Contact was successfully created." }
+        format.json { render :show, status: :created, location: @contact }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :contact_no, :message )
+  end
+end 
