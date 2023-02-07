8cc9ce2c16155fa573252ae2fe4fc2484397d29f
ashnew1226 <ashishdede05@gmail.com>
2023-02-07 17:37:09 +0530

    find profile of user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,36 +1,28 @@
 class ProfileController < ApplicationController
-    def index
-        @user = current_user
-        # binding        
-    end
-    
-    def new
-        @user = User.new
-    end
-    def edit
 
+    def edit
+        @profile = User.find(current_user.id)
     end
     
     def myorder
         @user_profile_image = current_user.image
         @my_order = current_user.user_orders
         @order_details = OrderDetail.all
-        # binding.pry
         
     end
     def update
+        @profile = User.find(params[:id])
         respond_to do |format|
-            if @contact.update(user_params)
-              format.html { redirect_to profile_url(@user), notice: "profile was successfully updated." }
-              format.json { render :show, status: :ok, location: @user }
+            if @profile.update(user_params)
+              format.html { redirect_to profile_url(@profile), notice: "profile was successfully updated." }
+              format.json { render :show, status: :ok, location: @profile }
             else
               format.html { render :edit, status: :unprocessable_entity }
-              format.json { render json: @user.errors, status: :unprocessable_entity }
+              format.json { render json: @profile.errors, status: :unprocessable_entity }
             end
-          end
+        end
     end
     def track_order
-        # binding.pry
         order = current_user.user_orders.all
         order_id = params[:user_order_id].to_i
         if (order.ids).include?(order_id)
@@ -49,13 +41,3 @@ private
 def user_params
     params.require(:user).permit( :firstname, :email, :lastname)    
 end
-
-
-# id = params[:user_order_id].to_i
-# 		if (order.ids).include?(id)
- 
-#       @user_order = UserOrder.find_by(id: id)
-#     elsif id != 0 && (order.ids).include?(id)
-#  == false  #if we convert nil to integer we get 0 so wrote 0 instead of nil
-#       flash[:alert] = "incorrect order id"
-#     end
