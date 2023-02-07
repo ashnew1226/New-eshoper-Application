5a0b98ef1c4ff7b2e7d9314d8fa1f03e5d649aa1
ashnew1226 <ashishdede05@gmail.com>
2023-02-07 17:19:57 +0530

    added code for invalid coupon condition

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,5 @@
 class CouponsController < ApplicationController
     
-
     def new
         @coupon = Coupon.all
     end
@@ -16,27 +15,22 @@ class CouponsController < ApplicationController
         @coupons.each do |coupon|
             if @used_coupon == coupon.code
                 
-                    if @user.coupons.exclude?(@cp)
-                        coupon_off = coupon.percent_off
-                        puts "******************valid coupon applied*******************************"
-                        coupon.number_of_uses += 1
-                        @total = cart_price - coupon_off
-                        @user.coupons << coupon
-                        binding.pry
-                        redirect_to cart_index_path(total: @total,coupon_off: coupon_off ), notice: "Coupon applied successfully." 
-                        # redirect_to coupons_url(@coupon)
-                        # binding.pry
-                    else
-                        puts "*****coupn already used************************"
-                        # binding.pry
-                        redirect_to cart_index_path,alert: "coupon allready used"
-                        binding.pry
-                        # format.json { render json: @coupon.errors, status: :unprocessable_entity }
-                        
-                    end
-                
+                if @user.coupons.exclude?(@cp)
+                    coupon_off = coupon.percent_off
+                    coupon.number_of_uses += 1
+                    @total = cart_price - coupon_off
+                    @user.coupons << coupon
+                    redirect_to cart_index_path(total: @total,coupon: coupon), notice: "Coupon applied successfully." 
+                    binding.pry
+                else
+                    redirect_to cart_index_path,alert: "coupon allready used"                        
+                end
+            else
+                flash[:alert] = "invalid coupon"                        
             end
+            
         end
+        redirect_to cart_index_path
     end
     def show
         
