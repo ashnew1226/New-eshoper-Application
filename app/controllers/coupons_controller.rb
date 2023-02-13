3723a82829c32c9e6be6cfd16bf734f8eeae27b6
ashnew1226 <ashishdede05@gmail.com>
2023-02-13 12:57:08 +0530

    removed extra method and space

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,40 +1,32 @@
 class CouponsController < ApplicationController
     
   def new
-      @coupon = Coupon.all
+    @coupon = Coupon.all
   end
 
   def apply_coupon
-    @user = current_user
-    @used_coupon = params[:code]
-    @cp = Coupon.find_by(code: @used_coupon)
-    @coupons = Coupon.all
+    user = current_user
+    used_coupon = params[:code]
+    available_coupon = Coupon.find_by(code: used_coupon)
+    coupons = Coupon.all
     number_of_uses = 0
     cart_price = params[:cart_sub_total].to_i
-    
-    @coupons.each do |coupon|
-      if @used_coupon == coupon.code
-          
-        if @user.coupons.exclude?(@cp)
-            coupon_off = coupon.percent_off
-            coupon.number_of_uses += 1
-            @total = cart_price - coupon_off
-            @user.coupons << coupon
-            redirect_to cart_index_path(total: @total,coupon: coupon), notice: "Coupon applied successfully." 
-            binding.pry
+    coupons.each do |coupon|
+      if used_coupon == coupon.code
+        if user.coupons.exclude(available_coupon)
+          coupon_off = coupon.percent_off
+          coupon.number_of_uses += 1
+          total = cart_price - coupon_off
+          user.coupons << coupon
+          session[:coupon] = coupon
+          redirect_to cart_index_path, notice: "Coupon applied successfully." 
         else
-            redirect_to cart_index_path,alert: "coupon allready used"                        
+          redirect_to cart_index_path, alert: "Coupon allready used"                        
         end
       else
-          flash[:alert] = "invalid coupon"                        
+        flash[:alert] = "Invalid coupon"                        
       end
-        
     end
-    redirect_to cart_index_path
-  end
-  def show
-      
   end
     
-
 end
