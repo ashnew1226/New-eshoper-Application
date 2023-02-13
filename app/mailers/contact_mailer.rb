125c47db926ca0792376174340a3b8a8bbf34ad4
ashnew1226 <ashishdede05@gmail.com>
2023-02-13 13:44:38 +0530

    removed extra space

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,14 @@
 class ContactMailer < ApplicationMailer
-    def contact_mail(current_user)
-        @user = current_user
-        @contact = params[:contact]
-        mail(to: @user.email, subject: "Your responce sent successfully.")
-    end
 
-    def contact_admin_mail(contact)
-        @contact = contact
-        mail(to: @contact.email, subject: "Your responce sent successfully.")
-    end
+	def contact_mail(current_user)
+		@user = current_user
+		@contact = params[:contact]
+		mail(to: @user.email, subject: "Your response sent successfully.")
+	end
+
+	def contact_admin_mail(contact)
+		@contact = contact
+		mail(to: @contact.email, subject: "Your response sent successfully.")
+	end
+	
 end
