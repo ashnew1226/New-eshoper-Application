1bfdf3fc0696261de0394aef27c1458d3de451b2
ashnew1226 <ashishdede05@gmail.com>
2023-02-13 12:42:11 +0530

    removed space

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,27 +1,27 @@
 class CheckoutController < ApplicationController
+
   def index
-    @final_price = params[:final_price]
-    if params[:coupon].present?
-      @coupon  = Coupon.find(params[:coupon])
-    end
-    if user_shipping_address.blank?
-    else
-        @address = user_shipping_address.join(" ")
+    products = Product.all
+    @products_purchase = @cart
+    @total_amount = session[:cart_hash]["products_price"].to_i
+    @products_subscription = products.where(name:nil, name:nil)
+    unless user_shipping_address.blank?
+      @address = user_shipping_address.join(" ")
     end
   end
 
   private
+
   def user_shipping_address
     @user_address = current_user.user_addresses.last
     @address = []
-    if @user_address.blank?
-    else
-        @address << @user_address.shipping_address
-        @address << @user_address.city
-        @address << @user_address.state
-        @address << @user_address.country
-        @address << @user_address.zipcode
-        @newaddress = @address
+    unless @user_address.blank?
+      @address << @user_address.shipping_address
+      @address << @user_address.city
+      @address << @user_address.state
+      @address << @user_address.country
+      @address << @user_address.zipcode
+      @newaddress = @address
     end
   end
 
