7cbb254a1441b5b5a56f7a7893d5f29c36adbc61
ashnew1226 <ashishdede05@gmail.com>
2023-02-13 13:05:00 +0530

    changes in indentation

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,5 @@
 class ProfileController < ApplicationController
-
+  
   def edit
     @profile = User.find(current_user.id)
   end
@@ -7,14 +7,13 @@ class ProfileController < ApplicationController
   def myorder
     @user_profile_image = current_user.image
     @my_order = current_user.user_orders
-    @order_details = OrderDetail.all
-      
+    @order_details = OrderDetail.all   
   end
+
   def update
-    @profile = User.find(params[:id])
     respond_to do |format|
       if @profile.update(user_params)
-        format.html { redirect_to profile_url(@profile), notice: "profile was successfully updated." }
+        format.html { redirect_to profile_url(@profile), notice: "Profile successfully updated." }
         format.json { render :show, status: :ok, location: @profile }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -22,16 +21,14 @@ class ProfileController < ApplicationController
       end
     end
   end
+
   def track_order
     order = current_user.user_orders.all
     order_id = params[:user_order_id].to_i
     if (order.ids).include?(order_id)
       @user_order = current_user.user_orders.find_by(id: order_id)
-      # binding.pry
     elsif order_id != 0 && (order.ids).include?(order_id) == false
       flash[:notice] = "Please enter correct order id."
-    else 
-
     end
   end
   
@@ -40,4 +37,5 @@ class ProfileController < ApplicationController
   def user_params
     params.require(:user).permit( :firstname, :email, :lastname)    
   end
+
 end
