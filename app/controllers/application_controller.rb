2e7bae751df22c531e29a73a8d324281ab2b5456
ashnew1226 <ashishdede05@gmail.com>
2023-02-13 12:34:53 +0530

    changes in indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,23 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :initailize_session
-    before_action :load_cart
-    before_action :cms_data
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :initailize_session
+  before_action :load_cart
+  before_action :cms_data
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-    
-    def cms_data
-      @cms = ContentManagementSystem.last
-    end
-    private
-
-    def initailize_session
-        session[:cart] ||= [] # empty cart = empty array
-    end
-
-    def load_cart
-        @cart = Product.find(session[:cart]) rescue nil
-        @cart_quantity = session[:cart_quantity]
-    end
-    
-    protected
-
+  def cms_data
+    @cms = ContentManagementSystem.last
+  end
+  private
+  
+  def initailize_session
+    session[:cart] ||= []
+  end
+  
+  def load_cart
+    @cart = Product.find(session[:cart]) rescue nil
+  end
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname])
   end
