ba3bf704a76771cc4aee37e28ddad4f97b8061aa
ashnew1226 <ashishdede05@gmail.com>
2023-02-16 15:12:29 +0530

    chenges at credentials for mailchimp service

--- app/services/mailchimp.rb
+++ app/services/mailchimp.rb
@@ -2,22 +2,22 @@ class Mailchimp
 	def self.execute(email:, fname:, lname:, addr1:, city:, state:, zip:)
 		client = MailchimpMarketing::Client.new()
 		client.set_config({
-			:api_key => '5bac1b07f0e243c9e9a44971ce8f4f79',
-			:server => 'us11'
+			:api_key => ENV['mailchimp_api_key'],
+			:server => ENV['DC_key']
 		})
-		list_id = "e6757e33e0"
+		list_id = ENV['mailchimp_audience_id']
 		result = client.lists.add_list_member list_id, 
 		{ email_address: email,
 		status: "subscribed",
 		merge_fields: 
 								{ FNAME: fname,
-								LNAME: lname,
-								ADDRESS: { addr1: addr1,
-													city: city,
-													state: state,
-													zip: zip,
-												},
-						},
+									LNAME: lname,
+									ADDRESS: { 	addr1: addr1,
+															city: city,
+															state: state,
+															zip: zip,
+														},
+								},
 		}
 
 	end
