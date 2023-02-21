6babb1068256587f087e82a77e85484ca8f41204
ashnew1226 <ashishdede05@gmail.com>
2023-02-21 10:29:18 +0530

    changes in checkout for selecting user addresses

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -5,24 +5,13 @@ class CheckoutController < ApplicationController
     @products_purchase = @cart
     @total_amount = session[:cart_hash]["products_price"].to_i
     @products_subscription = products.where(name:nil, name:nil)
-    unless user_shipping_address.blank?
-      @address = user_shipping_address.join(" ")
+    @address = current_user.user_addresses.select(:id,:shipping_address,:city,:state,:zipcode).last(5)
+    @addr_hashs = []
+    @address.each do |add|
+      my_address = "#{add.shipping_address}, #{add.city}, #{add.state} -  #{add.zipcode}"
+      @addr_hashs << {id: add.id , address: my_address}
     end
-  end
 
-  private
-
-  def user_shipping_address
-    @user_address = current_user.user_addresses.last
-    @address = []
-    unless @user_address.blank?
-      @address << @user_address.shipping_address
-      @address << @user_address.city
-      @address << @user_address.state
-      @address << @user_address.country
-      @address << @user_address.zipcode
-      @newaddress = @address
-    end
   end
 
 end
