da3fc60aa8f4e6ee511d889751c04b9135a40d06
ashnew1226 <ashishdede05@gmail.com>
2023-02-21 16:33:00 +0530

    removed unwanted files

--- app/models/user_address.rb
+++
@@ -1,8 +0,0 @@
-class UserAddress < ApplicationRecord
-	belongs_to :user
-	has_many :user_orders
-
-	validates :shipping_address,:city,:country,:state,:zipcode, presence: true
-	validates :zipcode, numericality: { only_integer: true }
-
-end
