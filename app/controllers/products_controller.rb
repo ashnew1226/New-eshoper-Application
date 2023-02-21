9480cc24cca03562f846d893d712532ff527c63a
ashnew1226 <ashishdede05@gmail.com>
2023-02-21 10:31:40 +0530

    added method for product price filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,15 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def filter
+    @products = Product.costs_less_than(params[:option])
   end
   
 end
