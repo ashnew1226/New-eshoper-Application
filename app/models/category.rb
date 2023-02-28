58c188213e7bb84f58f9be398f3c097bc1eab813
ashnew1226 <ashishdede05@gmail.com>
2023-02-28 11:14:56 +0530

    changes at model for parent of category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
 	has_many :product_categories
 	has_many :products, through: :product_categories, dependent: :destroy
 	has_many :subcategories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
-	belongs_to :parent_category, class_name: "Category", optional: true
+	belongs_to :parent, class_name: "Category", optional: true
 	enum :status, [ :active, :inactive]
 end
