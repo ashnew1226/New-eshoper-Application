06092ab12a268d057fc047c34ca53e1d77af0781
ashnew1226 <ashishdede05@gmail.com>
2023-02-28 20:11:11 +0530

    added scopes for recent_customer queries

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,10 +3,12 @@ class Contact < ApplicationRecord
 	after_update :send_admin_response
 	validates :name, :email,:message, presence: true
 	validates :contact_no, length: { is: 10 }
-
+	
 	scope :recent_customer_queries, -> { where("DATE(created_at) = ?",Date.today) }
-	scope :recent_customer_queries_admin_replied, -> { recent_customer_queries.where.not(note_admin: nil) }
+	scope :recent_replied_queries, -> { recent_customer_queries.where.not(note_admin: nil) }
+
 	def send_admin_response
 		ContactMailer.contact_admin_mail(self).deliver_now
 	end
+
 end
