2e668b58d5ea8e87f49bcdf3b30579c51d7f5708
ashnew1226 <ashishdede05@gmail.com>
2023-02-28 11:18:37 +0530

    changes for scopes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,11 +3,9 @@ class Contact < ApplicationRecord
 	after_update :send_admin_response
 	validates :name, :email,:message, presence: true
 	validates :contact_no, length: { is: 10 }
-	binding.pry
 
 	scope :recent_customer_queries, -> { where("DATE(created_at) = ?",Date.today) }
 	scope :recent_customer_queries_admin_replied, -> { recent_customer_queries.where.not(note_admin: nil) }
-	binding.pry
 	def send_admin_response
 		ContactMailer.contact_admin_mail(self).deliver_now
 	end
