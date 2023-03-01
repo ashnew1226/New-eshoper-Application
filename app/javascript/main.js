b4c956001e19b5aad84b49750f6d6119700573d8
ashnew1226 <ashishdede05@gmail.com>
2023-03-01 20:21:18 +0530

    javascript to show selected range value

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -64,3 +64,14 @@ function buttonFunc() {
 	$("#ex8").slider({
     tooltip: 'always'
 });
+
+
+var elem = document.querySelector('input[type="range"]');
+
+var rangeValue = function(){
+  var newValue = elem.value;
+  var target = document.querySelector('.value');
+  target.innerHTML = newValue;
+}
+
+elem?.addEventListener("input", rangeValue);
