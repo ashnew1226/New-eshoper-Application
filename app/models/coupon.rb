77812924b1720a751b2dac9a0fa967028494a7da
ashnew1226 <ashishdede05@gmail.com>
2023-03-02 12:02:47 +0530

    added dependent destroy

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,6 @@
 class Coupon < ApplicationRecord
 	has_many :coupons_useds
-	has_many :orders
-	has_many :users, through: :coupons_useds
+	has_many :orders, dependent: :destroy
+	has_many :users, through: :coupons_useds, dependent: :destroy
 
 end
