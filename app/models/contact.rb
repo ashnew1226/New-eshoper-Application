52d9c2c9afa7c1f59466ff0f6973b97c31c0006e
ashnew1226 <ashishdede05@gmail.com>
2023-03-02 12:01:49 +0530

    removed name and email and changes in scope

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,11 @@
 class Contact < ApplicationRecord
 	belongs_to :user
 	after_update :send_admin_response
-	validates :name, :email,:message, presence: true
+	validates :message, presence: true
 	validates :contact_no, length: { is: 10 }
 	
 	scope :recent_customer_queries, -> { where("DATE(created_at) = ?",Date.today) }
-	scope :recent_replied_queries, -> { recent_customer_queries.where.not(note_admin: nil) }
+	scope :recent_replied_queries, -> { recent_customer_queries.where("note_admin IS NOT NULL") }
 
 	def send_admin_response
 		ContactMailer.contact_admin_mail(self).deliver_now
