3d6ef6863b9ee03806b5a573024ff49d91942f84
ashnew1226 <ashishdede05@gmail.com>
2023-03-02 12:05:46 +0530

    scope for search functionality of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,21 +12,5 @@ class Product < ApplicationRecord
 	has_one_attached :ProductImage
 	validates :quantity, numericality: { greater_than_or_equal_to: 1 }
   scope :costs_less_than, ->(price) { where("price < ?", price) }
-  scope :costs_more_than, ->(price) { where(" price > ?", price) }
-	scope :recent, -> (value) { where("created_at") }
-  # scope :cost_between, ->(low_price, high_price){where(price: low_price..high_price)}
-
-	def self.filter(cost)
-		# binding.pry
-		if cost[:option].present? && cost[:dec_option].present?
-			# a = self.costs_less_than(cost[:option]) && self.costs_more_than_less(cost[:dec_option])
-			# self.cost_between(cost[:option], cost[:dec_option])
-		elsif cost[:option].present?
-			self.costs_less_than(cost[:option])
-		elsif cost[:dec_option].present?
-			self.costs_more_than(cost[:dec_option])
-		end
-		# self.costs_more_than(cost) if cost[:dec_option].present?
-	end
-
+	scope :search, ->(key){where("name ILIKE ? or short_description ILIKE ? or long_description ILIKE ?", key,key,key)}
 end
