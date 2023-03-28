4ba6cce4f3966c42200d293143144fc57108611e
ashnew1226 <ashishdede05@gmail.com>
2023-03-28 10:21:54 +0530

    changes after production in local succesfull

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :initailize_session
   before_action :load_cart
-  before_action :cms_data
+  # before_action :cms_data
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :admin_user
-  def cms_data
-    @cms = ContentManagementSystem.last
-  end
+  # def cms_data
+  #   @cms = ContentManagementSystem.last
+  # end
   private
   
   def admin_user
